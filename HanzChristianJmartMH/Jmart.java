5974eeb69694534b83d3a982501e190eccb60cc8
HanzChristian <hanz.christian@ui.ac.id>
2021-09-11 10:58:50 +0700

    final

--- HanzChristianJmartMH/Jmart.java
+++ HanzChristianJmartMH/Jmart.java
@@ -2,6 +2,68 @@ package HanzChristianJmartMH;
 
 public class Jmart
 {
-
-   
+    public static void main(String args[]){
+        System.out.println("getPromo : " + getPromo());
+    }
+    
+    public static int getPromo(){
+        return 0;
+    }
+    
+    public static String getCustomer(){
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage(int before, int after){
+        if(before<after)
+        {
+            return 0.0f;
+        }
+        else
+        {
+            int selisih = before-after;
+            float percentage = (selisih/before)*100;
+            return before-after;
+        }
+    }
+    
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        if(discountPercentage > 100.0f)
+        {
+            return 0;
+        }
+        else{
+            float harga = price;
+            float discount = price - ((discountPercentage*harga)/100);
+            int hargaakhir = (int) discount;
+            return hargaakhir;
+        }
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        float discprice = discountedPrice;
+        float hargaoriginal = (1/(1-(discountPercentage))) * discprice;
+        int originalint = (int) hargaoriginal;
+        return originalint;
+    }
+    
+    public static float getComissionMultiplier(){
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice(int price){
+        float besarcomission = getComissionMultiplier();
+        float adjustprice = price + (besarcomission*price);
+        int adjustpriceint = (int) adjustprice;
+        return adjustpriceint;
+    }
+    
+    public static int getAdminFee(int price){
+        float floatprice = price;
+        float finalfee = floatprice*getComissionMultiplier();
+        int adminfee = (int)finalfee;
+        return adminfee;
+    }
+    
+    
 }
