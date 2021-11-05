14dddaef0b0aeadd22302d4f1aa67f90790f66d7
HanzChristian <hanz.christian@ui.ac.id>
2021-11-05 16:24:17 +0700

    TP Modul 6 - Hanz Christian

--- src/HanzChristianJmartMH/Jmart.java
+++ src/HanzChristianJmartMH/Jmart.java
@@ -1,15 +1,33 @@
 package HanzChristianJmartMH;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 public class Jmart
 {
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String args[])
     {
-        //print terhadap hasil
-    	System.out.println("Hello from Eclipe!");
-
+        String filepath = "D:/Perkuliahan/Semester 5/Praktikum/OOP/Modul 1/Folder khusus/jmart/src/city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
 /* public class Jmart
