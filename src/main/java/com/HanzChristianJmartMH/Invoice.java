6b77991ed710276e17124f909ccb4778457f0b19
HanzChristian <hanz.christian@ui.ac.id>
2021-11-20 11:14:52 +0700

    CS Modul 8 - Hanz Christian

--- src/main/java/com/HanzChristianJmartMH/Invoice.java
+++ src/main/java/com/HanzChristianJmartMH/Invoice.java
@@ -16,7 +16,7 @@ public abstract class Invoice extends Serializable
     enum Rating{
         NONE, BAD, NEUTRAL, GOOD;
     }
-    enum Status{
+    public enum Status{
         WAITING_CONFIRMATION, CANCELLED, ON_PROGRESS, ON_DELIVERY, COMPLAINT, FINISHED,FAILED,DELIVERED;
     }
     
--- src/main/java/com/HanzChristianJmartMH/controller/PaymentController.java
+++ src/main/java/com/HanzChristianJmartMH/controller/PaymentController.java
@@ -1,10 +1,16 @@
 package com.HanzChristianJmartMH.controller;
 
+import com.HanzChristianJmartMH.Invoice;
 import com.HanzChristianJmartMH.ObjectPoolThread;
 import com.HanzChristianJmartMH.Payment;
+import com.HanzChristianJmartMH.Shipment;
 import com.HanzChristianJmartMH.controller.BasicGetController;
 import com.HanzChristianJmartMH.dbjson.JsonTable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class PaymentController implements BasicGetController<Payment> {
     public static final long DELIVERED_LIMIT_MS = 6;
@@ -26,8 +32,12 @@ public class PaymentController implements BasicGetController<Payment> {
     }
 
     @PostMapping("/create")
-    public Payment create(int buyerId, int productId, int productCount, String shipmentAddress, byte shipmentPlan){
-        return null;
+    public Payment create(@RequestParam int buyerId,@RequestParam int productId,@RequestParam int productCount,@RequestParam String shipmentAddress, @RequestParam byte shipmentPlan){
+        Shipment shipment = new Shipment(shipmentAddress, 500, shipmentPlan, "");
+        Payment payment = new Payment(buyerId, productId, productCount, shipment);
+
+        paymentTable.add(payment);
+        return payment;
     }
 
     public JsonTable<Payment> getJsonTable(){
@@ -36,10 +46,31 @@ public class PaymentController implements BasicGetController<Payment> {
 
     @PostMapping("/{id}/submit")
     public boolean submit(int id,String receipt){
-        return false;
+        return true;
     }
 
     private static boolean timekeeper(Payment payment){
+//        Date timeNow = Calendar.getInstance().getTime();
+//        if(payment.history.size() != 0){
+//            Payment.Record lastRecord = payment.history.get(payment.history.size() - 1);
+//            long timePassed = timeNow.getTime() - lastRecord.date.getTime();
+//            if(lastRecord.status == Invoice.Status.WAITING_CONFIRMATION && (timePassed > WAITING_CONF_LIMIT_MS)){
+//                payment.history.add(new Payment.Record(Invoice.Status.FAILED, "Gagal"));
+//                return true;
+//            }
+//            else if((lastRecord.status == Invoice.Status.ON_PROGRESS) && (timePassed > ON_PROGRESS_LIMIT_MS)){
+//                payment.history.add(new Payment.Record(Invoice.Status.FAILED, "Gagal"));
+//                return true;
+//            }
+//            else if(lastRecord.status == Invoice.Status.ON_DELIVERY && timePassed > ON_DELIVERY_LIMIT_MS){
+//                payment.history.add(new Payment.Record(Invoice.Status.DELIVERED, "TERKIRIM"));
+//                return true;
+//            }
+//            else if(lastRecord.status == Invoice.Status.DELIVERED && timePassed > DELIVERED_LIMIT_MS){
+//                payment.history.add(new Payment.Record(Invoice.Status.FINISHED, "SELESAI"));
+//                return true;
+//            }
+//        }
         return false;
     }
 }
