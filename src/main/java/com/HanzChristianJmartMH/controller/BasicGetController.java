95b5c8a7a98aa4e4eab96507fa6ebfea99ba690b
HanzChristian <hanz.christian@ui.ac.id>
2021-12-05 13:10:01 +0700

    TUTAM Modul 9 - Hanz Christian

--- src/main/java/com/HanzChristianJmartMH/controller/BasicGetController.java
+++ src/main/java/com/HanzChristianJmartMH/controller/BasicGetController.java
@@ -12,14 +12,14 @@ import java.util.List;
 @RestController
 public interface BasicGetController <T extends Serializable>{
     @GetMapping("/id")
-    default T getById (@PathVariable int id){
+    default T getById  (int id){
         return Algorithm.<T>find(getJsonTable(),(e) -> e.id == id);
     }
 
     public abstract JsonTable<T> getJsonTable();
 
     @GetMapping("/page")
-    default List<T> getPage (@PathVariable int page, @PathVariable int pageSize){
+    default List<T> getPage (int page,  int pageSize){
         return Algorithm.<T>paginate(getJsonTable(),page,pageSize,e -> true);
     }
 }
--- src/main/java/com/HanzChristianJmartMH/controller/ProductController.java
+++ src/main/java/com/HanzChristianJmartMH/controller/ProductController.java
@@ -1,56 +1,39 @@
 package com.HanzChristianJmartMH.controller;
 
 import com.HanzChristianJmartMH.*;
+import com.HanzChristianJmartMH.dbjson.JsonAutowired;
 import com.HanzChristianJmartMH.dbjson.JsonTable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import com.HanzChristianJmartMH.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductController implements BasicGetController<Product>{
+@RestController
+@RequestMapping("/product")
+public class ProductController implements BasicGetController<Product> {
+    @JsonAutowired(value = Product.class, filepath = "D:\\Perkuliahan\\Semester 5\\Praktikum\\OOP\\Modul 1\\Folder khusus\\jmart\\src\\GoldenSample\\store.json")
     public static JsonTable<Product> productTable;
 
-    @PostMapping("/product/create")
-    Product create(@RequestParam int accountId, @RequestParam String name, @RequestParam int weight, @RequestParam boolean conditionUsed, @RequestParam double price, @RequestParam double discount, @RequestParam ProductCategory category, @RequestParam byte shipmentPlans){
-        for(Account acc : AccountController.accountTable){
-            if (acc.store == null || acc.id != accountId) {
-            } else {
-                Product newProduct = new Product(accountId, discount, price, shipmentPlans, name, weight, conditionUsed, category);
-                productTable.add(newProduct);
-                return newProduct;
+    public JsonTable<Product> getJsonTable() {
+        return productTable;
+    }
+
+    @GetMapping("/{id}/store")
+    List<Product> getProductByStore(@PathVariable int id, @RequestParam int page, @RequestParam int pageSize) {
+        return Algorithm.<Product>paginate(getJsonTable(), page, pageSize, product -> product.accountId == id);
+    }
+
+    @PostMapping("/create")
+    Product create(@RequestParam int accountId, @RequestParam String name, @RequestParam int weight, @RequestParam boolean conditionUsed,
+                   @RequestParam double price, @RequestParam double discount, @RequestParam ProductCategory category, @RequestParam byte shipmentPlans) {
+        for (Account account : AccountController.accountTable) {
+            if (account.id == accountId && account.store != null) {
+                Product p = new Product(accountId, discount, price, shipmentPlans, name, weight, conditionUsed, category);
+                productTable.add(p);
+                return p;
             }
         }
         return null;
     }
-
-    @PostMapping("/product")
-    public JsonTable<Product> getJsonTable(){
-        return productTable;
-    }
-
-    @PostMapping("/product/{id}/store")
-    List<Product> getProductByStore(int id, int page, int pageSize){
-        return Algorithm.paginate(productTable, page, pageSize, pred->pred.accountId == id);
-    }
-
-    @GetMapping("/getFiltered")
-    List<Product> getProductFiltered(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize, @RequestParam(defaultValue = "0") int accountId, @RequestParam(defaultValue = "") String search, @RequestParam(defaultValue = "0.0") int minPrice, @RequestParam(defaultValue = "0.0") int maxPrice, @RequestParam(defaultValue = "") ProductCategory category){
-        List<Product> listed = new ArrayList<>();
-        if(accountId != 0){
-            listed = Jmart.filterByAccountId(listed, accountId, page, pageSize);
-        }
-        if(!search.equals("")){
-            listed = Jmart.filterByName(listed, search, page, pageSize);
-        }
-        if(minPrice != 0.0 || maxPrice != 0.0){
-            listed = Jmart.filterByPrice(listed, minPrice, maxPrice);
-        }
-        if(category != null){
-            listed = Jmart.filterByCategory(listed, category);
-        }
-        return listed;
-    }
-
 }
